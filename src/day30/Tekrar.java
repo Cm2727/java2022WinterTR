c76510ea8634c3884e6c445f32e3470a25852a03
Cuma <cuma.ozturkmen@gmail.com>
2022-03-31 11:25:47 +0200

    31.03.2022 v1

---
+++ src/day30/Tekrar.java
@@ -0,0 +1,20 @@
+package day30;
+
+public class Tekrar {
+    public static void main(String[] args) {
+
+        String cumele="Kahve ıcmek guzel";
+
+        for (int i =cumele.length()-1; i >0 ; i--) {
+
+            System.out.println(i+ "-" + cumele );
+
+        }
+
+
+    }
+
+
+
+
+}
