3c2d5bd75aaf1cb7dd7bf7f9a01d33194793849c
Cm2727 <cuma.ozturkmen@gmail.com>
2022-04-07 23:26:47 +0200

    07.04.2022

--- src/day18_nestedForLoop/C02_NestedForLoop.java
+++ src/day18_nestedForLoop/C02_NestedForLoop.java
@@ -4,7 +4,7 @@ public class C02_NestedForLoop {
     public static void main(String[] args) {
         //kullanicidan bir rakam alip carpim tablosu oolusturalim
 
-        int input = 3;
+        int input = 5;
         //1 2 3   1*1 1*2 1*3
         //2 4 6    2*1 2*2 2*3
         //3 6 9   3*1 3*2 3*3
---
+++ src/day37_inheritance/Child.java
@@ -0,0 +1,20 @@
+package day37_inheritance;
+
+import day36_inheritance.Parent;
+
+public class Child extends Parent {
+
+    Child(){
+
+        super();
+        System.out.println("child class parametresiz cons");
+
+    }
+
+    public static void main(String[] args) {
+
+        
+    }
+
+
+}
